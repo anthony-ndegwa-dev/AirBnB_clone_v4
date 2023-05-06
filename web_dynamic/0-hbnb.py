32b174b102426eaab35dafee2b58ed350ebe0f93
root <anthony.n.ndegwa@gmail.com>
2023-05-06 07:54:25 -0700

    Modify script

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Flask app generates html page containing location/amenity
+"""Script generates html page containing location/amenity
 dropdown menus and rental listings"""
 from flask import Flask, render_template
 from models import storage
-app = Flask('web_flask')
+import uuid
+app = Flask('web_dynamic')
 app.url_map.strict_slashes = False
 
 
-@app.route('/0-hbnb/')
+@app.route('/0-hbnb')
 def display_hbnb():
     """Generate page with popdown menu of states/cities"""
     states = storage.all('State')
     amenities = storage.all('Amenity')
     places = storage.all('Place')
+    cache_id = uuid.uuid4()
     return render_template('0-hbnb.html',
                            states=states,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 @app.teardown_appcontext
